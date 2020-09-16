a01a5b2bbd9e60d02e9cd1d8c17a35ee008b0094
Myzwer <josh@onefortyfivedesign.com>
2020-09-16 01:53:14 -0400

    Finished the header and start of the body on about us and removed a block of commented code from header.php.

---
+++ devwp/about.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Template Name: About Template
+ *
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ *
+ * @package WordPress
+ * @subpackage Charis
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+<div class="grid-container full-width">
+    <div class="grid-x grid-padding-x full-background" style = "background: url(https://images.unsplash.com/photo-1516426122078-c23e76319801?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2248&q=80);  background-position: center center;">
+        <div class="small-12 medium-6 medium-offset-6 cell">
+            <div class="massive-margin">
+            <h4 class = "hero-small-font no-margin no-padding">We are</h4>
+            <h1 class = " no-margin no-padding">Charis in Action</h1>
+            <p class = "no-margin no-padding primary-invert italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae commodi consequatur dignissimos distinctio doloribus minima molestias necessitatibus nulla. Aliquam consectetur eum exercitationem, fugit ipsam ipsum nobis provident ratione sint sunt!</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <div class="full-width color-background">
+        <div class = "grid-container">
+            <div class="grid-x grid-padding-x grid-margin-x">
+                <div class="small-12 cell margin-top margin-bottom">
+                    <h2>"Go therefore and make disciples of all nations, baptizing them in the name of the Father and of the Son and of the Holy Spirit."</h2>
+                    <h4 class = "right">Matthew 28:19</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="full-width main-background">
+        <div class = "grid-container">
+            <div class="grid-x grid-padding-x grid-margin-x">
+                <div class="small-12 cell margin-top margin-bottom">
+                    <h4 class = "background-invert">Charis in Action was founded in 2014 to support external ministries outside the local church.</h4>
+                    <p class = "no-margin no-padding">Charis is the Greek word for grace, God's undeserved favor that He provides for mankind. Charis in Action equips disciples of Jesus to both share and demonstrate the love of Jesus Christ. It is literally God's grace in action throughout the world. The foundation of everything we do is rooted in the Great Commission: “Go and make disciples of all nations” Matthew 28:19. It is our passion to make Jesus known to every nation, tribe, people, and language. Therefore, we partner with, equip, and send missional servants to faithfully carry out the Great Commission. We believe the local church is the primary biblical sending organization. However, some churches and church leaders do not understand or embrace this commitment. We want to help followers of Jesus have a pathway to engage in the global mission of God.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<?php get_footer();
