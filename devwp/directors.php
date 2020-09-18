51222368faf3f5cead954cf3d6cf5ad9f4fcab91
Myzwer <josh@onefortyfivedesign.com>
2020-09-18 06:41:39 -0400

    Completed the initial design of the directors page. Images still need to be updated, but the page itself structurally is done.

---
+++ devwp/directors.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Template Name: Directors Template
+ *
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ *
+ * @package WordPress
+ * @subpackage Charis
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+    <div class="full-width color-background">
+        <div class = "grid-container">
+            <div class="grid-x grid-padding-x grid-margin-x">
+                <div class="small-12 cell margin-top margin-bottom">
+                    <h1 class = "header-margin">Board of Directors</h1>
+                    <p class = "no-padding no-margin primary-invert">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque fugiat molestiae praesentium? Eius et explicabo minima nam praesentium repellat sed suscipit voluptatum. Accusamus aliquam in maiores perspiciatis quidem reprehenderit voluptates!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="full-width main-background">
+        <div class = "grid-container">
+            <div class="grid-x gray-background margin-top margin-bottom add-shadow rounded-corners">
+                <div class="small-12 medium-4 cell">
+                    <img class = "rounded-corner-img" src="https://images.unsplash.com/photo-1534308143481-c55f00be8bd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2976&q=80" alt="">
+                    <h3 class = "background-invert no-margin no-padding left-padding">Mark Bishop</h3>
+                    <h4 class = "background-invert left-padding">President</h4>
+                </div>
+                <div class="small-12 medium-8 cell">
+                    <p class = "add-padding no-top-padding">Leading by example both professionally and in ministry, Mark appreciates opportunities to motivate others to be on mission for Jesus Christ. A member of the Charis in Action board since 2010, Mark’s steady demeanor and heart for discipleship complement his experience as a trusted leader. Mark is a graduate of the University of Tennessee and works as a credit supervisor with 21st Mortgage Corporation. His desire to spread the gospel always forefront, Mark has traveled both domestically and internationally, ministering on short-term trips in Montana, Haiti, Asia, and Central America as he serves to make disciples of all nations. Mark invests in the lives of co-workers and men seeking to grow in their understanding of who they are in Christ, walking alongside them in one-on-one mentoring relationship while discipling them to grow in spiritual maturity. Mark and his wife, Criston, are the parents of three teenage children and make their home in Knoxville, Tennessee.</p>
+                </div>
+            </div>
+
+            <div class="grid-x gray-background margin-top margin-bottom add-shadow rounded-corners">
+                <div class="small-12 medium-4 cell">
+                    <img class = "rounded-corner-img" src="https://images.unsplash.com/photo-1534308143481-c55f00be8bd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2976&q=80" alt="">
+                    <h3 class = "background-invert no-margin no-padding left-padding">W. Stewart Ritchie</h3>
+                    <h4 class = "background-invert left-padding">Executive Director</h4>
+                </div>
+                <div class="small-12 medium-8 cell">
+                    <p class = "add-padding no-top-padding">Stewart is an accomplished leader who is passionate about helping organizations and people develop, grow and reach their full potential. During his twenty-five year career he has served as President and Chief Executive Officer for organizations. He holds an MBA from The University of Tennessee and serves on the board for The University of Tennessee School of Management. Stewart has served on the Charis In Action board since 2010. During his time with Charis, Stewart has led short term mission trips to Haiti, served in leadership positions at his church, and co hosted a talk show on Christian radio in Knoxville Tennessee. Outside of work, he and his wife enjoy spending time with their four children and are passionate University of Tennessee fans.</p>
+                </div>
+            </div>
+
+            <div class="grid-x gray-background margin-top margin-bottom add-shadow rounded-corners">
+                <div class="small-12 medium-4 cell">
+                    <img class = "rounded-corner-img" src="https://images.unsplash.com/photo-1534308143481-c55f00be8bd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2976&q=80" alt="">
+                    <h3 class = "background-invert no-margin no-padding left-padding">Matt White</h3>
+                    <h4 class = "background-invert left-padding">Something Awesome</h4>
+                </div>
+                <div class="small-12 medium-8 cell">
+                    <p class = "add-padding no-top-padding">Matt White joined the Charis in Action board in 2018 and serves in the role of treasurer. Matt's education and professional experience are in the fields of accounting, finance, and taxation. He studied at the University of Tennessee, earning his bachelor's degree and a master's degree in accountancy with a focus in taxation. He is a Certified Public Accountant and a CERTIFIED FINANCIAL PLANNER™ professional. Matt loves spending time with his wife, Sarah, and their two girls and enjoys reading, music, and sports. He makes his home in Maryville, TN.</p>
+                </div>
+            </div>
+
+            <div class="grid-x gray-background margin-top margin-bottom add-shadow rounded-corners">
+                <div class="small-12 medium-4 cell">
+                    <img class = "rounded-corner-img" src="https://images.unsplash.com/photo-1534308143481-c55f00be8bd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2976&q=80" alt="">
+                    <h3 class = "background-invert no-margin no-padding left-padding">Janet Black</h3>
+                    <h4 class = "background-invert left-padding">Secretary</h4>
+                </div>
+                <div class="small-12 medium-8 cell">
+                    <p class = "add-padding no-top-padding">With over 30 years of work experience, Janet is a passionate leader who inspires others to recognize their potential and empowers them to exceed their dreams. Her work, while predominantly domestic, provides international experience as well. Janet’s missional interests include serving in a variety of leadership roles at her local church, giving back to the community by volunteering at several ministries, and discipling and leading teams through short-term international mission trips in Africa, Haiti, Honduras, and Brazil. She has served on the Board of Charis in Action since 2015. Outside of work, she and her husband David enjoying spending time with their 2 grown children and their spouses, and cheering on her alma mater, University of Tennessee.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+<?php get_footer();
