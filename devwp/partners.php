e84e51ed9537bc686f5016254601c063ca496c13
Myzwer <josh@onefortyfivedesign.com>
2020-09-22 01:01:20 -0400

    Main structure of the partners page is done. Will need to update the content at a later point.

---
+++ devwp/partners.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Template Name: Partners Template
+ *
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ *
+ * @package WordPress
+ * @subpackage Charis
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+    <div class="grid-container full-width">
+        <div class="grid-x grid-padding-x full-background" style = "background: linear-gradient(
+      rgba(0, 0, 0, 0.3),
+      rgba(0, 0, 0, 0.3)
+    ), url(https://images.unsplash.com/photo-1553775927-a071d5a6a39a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2178&q=80);  background-position: top center;">
+            <div class="small-12 medium-6 medium-offset-1 cell">
+                <div class="massive-margin">
+                    <h4 class = "hero-small-font no-margin no-padding">We are</h4>
+                    <h1 class = " no-margin no-padding">Better Together</h1>
+                    <p class = "no-margin no-padding primary-invert italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae commodi consequatur dignissimos distinctio doloribus minima molestias necessitatibus nulla. Aliquam consectetur eum exercitationem, fugit ipsam ipsum nobis provident ratione sint sunt!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="full-width blue-background">
+        <div class = "grid-container">
+            <div class="grid-x grid-padding-x grid-margin-x">
+                <div class="small-12 cell margin-top margin-bottom">
+                    <h2>"Go therefore and make disciples of all nations, baptizing them in the name of the Father and of the Son and of the Holy Spirit."</h2>
+                    <h4 class = "right">Matthew 28:19</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="full-width gray-background top-bot-padding">
+        <div class = "grid-container">
+            <div class="grid-x grid-margin-x">
+                <div class="small-12 medium-6 about-card margin-bottom cell">
+                    <img src="https://images.unsplash.com/photo-1444664361762-afba083a4d77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80" alt="">
+                    <h2 class = "background-invert">Todd Stewart - Missionary, Zambia, Africa</h2>
+                    <p class = "">Todd was born in Columbus, Ohio and has served in vocational ministry since 1987. He received his undergraduate degree in business from Cumberland College (currently the University of the Cumberlands) in Williamsburg, Ky. He received his Master of Divinity and Doctor of Ministry degrees from the Southern Baptist Theological Seminary in Louisville, Ky. Todd has been in full-time since 1987 and has served as Student Pastor, Senior Pastor, and Missions Pastor. He has been married to Kristi since 1992 and have they three adult children: Trey and his wife Rebeccah, Kendra, and Karis. Todd and Kristi currently serve in Lusaka, Zambia as IMB missionaries.</p>
+                    <div class="margin-bottom">
+                    <button class="btn btn-v1 center">View All >></button>
+                    </div>
+                </div>
+
+                <div class="small-12 medium-6 about-card margin-bottom cell">
+                    <img src="https://images.unsplash.com/photo-1444664361762-afba083a4d77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80" alt="">
+                    <h2 class = "background-invert">IMB</h2>
+                    <p class = "">Exists to to serve Southern Baptists in carrying out the Great Commission to make disciples of all nations. IMB has one goal: bring the gospel of Jesus Christ to the lost peoples of the world. Southern Baptist churches believe that by working cooperatively, they can accomplish more for God’s kingdom. In recent years IMB has shifted focus from geography to people groups with a concerted effort to start church planting movements among unreached peoples. Technological advances have provided unprecedented opportunities to spread the gospel into areas previously considered forbidden by political barriers. Every believer has a part to play in the Great Commission, and IMB is dedicated to forging the pathways necessary to connect each church with the work of missionary teams across the globe.</p>
+                    <div class="margin-bottom">
+                        <button class="btn btn-v1 center">View All >></button>
+                    </div>
+                </div>
+
+                <div class="small-12 medium-6 about-card margin-bottom cell">
+                    <img src="https://images.unsplash.com/photo-1444664361762-afba083a4d77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80" alt="">
+                    <h2 class = "background-invert">IMB</h2>
+                    <p class = "">Exists to reach North America and the nations with the hope of the gospel through Church Planting, Compassion Ministry, and Evangelism Strategy. Church planting isn’t just a ministry for individual church planters; it’s a ministry for the whole Church. NAMB provides resources and stands alongside those who are called to plant churches. When working within the community, bridges are built, and relationships are created. When crisis arise, NAMB’s focus on compassion ministry meets the need, builds the relationship, and changes a life. Fulfilling the Great Commission can be an intimidating idea, but NAMB has practical resources to help prepare you and your church to live on mission.</p>
+                    <div class="margin-bottom">
+                        <button class="btn btn-v1 center">View All >></button>
+                    </div>
+                </div>
+
+                <div class="small-12 medium-6 about-card margin-bottom cell">
+                    <img src="https://images.unsplash.com/photo-1444664361762-afba083a4d77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80" alt="">
+                    <h2 class = "background-invert">Want to Learn More?</h2>
+                    <p class = "">All of our partners are doing fantastic work around the globe. If you'd like to stay as current and up to date as possible on what Todd Stewart, the founder of Charis is doing, you can read his blog posts or sign up for the email list.</p>
+                    <div class="margin-bottom">
+                        <button class="btn btn-v1 center">View All >></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="full-width color-background">
+        <div class = "grid-container">
+            <div class="grid-x grid-padding-x grid-margin-x">
+                <div class="small-12 cell margin-top margin-bottom">
+                    <h2>"Go therefore and make disciples of all nations, baptizing them in the name of the Father and of the Son and of the Holy Spirit."</h2>
+                    <h4 class = "right">Matthew 28:19</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+<?php get_footer();
