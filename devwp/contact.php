74e9ebdf837b83c7c44eeeb971a9988e0da2d504
Myzwer <josh@onefortyfivedesign.com>
2020-09-22 22:23:51 -0400

    Base structure of a contact is done. Page is pretty simple, but if the client wants to add to the about us section in the bottom more stuff might need to be added.

--- devwp/contact.php
+++ devwp/contact.php
@@ -14,14 +14,19 @@
 get_header(); ?>
 
 
+
     <div class="full-width main-background">
         <div class = "grid-container">
-            <div class="grid-x grid-padding-x padding-outer">
-                <div class="small-12 center cell">
-                    <h3>Contact</h3>
-                    <p>Look at my form, my form is amazing.</p>
+            <div class="grid-x grid-padding-x grid-margin-x">
+                <div class="small-12 medium-6 cell massive-margin">
+                    <h3 class = "no-margin no-padding background-invert">Get in Touch</h3>
+                    <p class = "no-padding">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad neque, voluptate! Libero molestiae nobis quas sed? Ab aut ea error est iure recusandae sed. Ducimus ipsa recusandae ut? Est, quod?</p>
+                    <p class = "no-padding"><i class="far fa-envelope"></i> <span class = "no-margin no-padding">admin@charisinaction.org</span></p>
+                    <p class = "no-padding"><i class="far fa-envelope"></i> <span class = "no-margin no-padding">stewartsinafrica@gmail.com</span></p>
+
                 </div>
-                <div class="small-12 medium-10 cell">
+                <div class="small-12 medium-6 cell gray-background rounded-corners add-shadow margin-bottom add-padding tablet-massive-margin">
+                    <h3 class = "no-margin no-padding background-invert">Say Something</h3>
                     <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
                         the_content();
                     endwhile; else: ?>
@@ -32,5 +37,15 @@ get_header(); ?>
         </div>
     </div>
 
+    <div class="full-width margin-pull color-background">
+        <div class = "grid-container">
+            <div class="grid-x grid-padding-x grid-margin-x">
+                <div class="small-12 cell margin-top margin-bottom-large">
+                    <h1 class = "header-margin">About Charis in Action</h1>
+                    <p class = "no-padding no-margin primary-invert">We partner with, equip, and send missional servants to faithfully carry out the Great Commission. We believe the local church is the primary biblical sending organization. However, some churches and church leaders do not understand or embrace this commitment. We want to help followers of Jesus have a pathway to engage in the global mission of God.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <?php get_footer();
